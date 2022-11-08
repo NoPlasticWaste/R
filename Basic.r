a0392cfcdf6804cc057cbee2c575dc1e3f8028cb
Vivien <endarlan11@gmail.com>
2022-11-08 15:13:55 +0700

    belajar mean dan median

--- Basic.r
+++ Basic.r
@@ -1,5 +1,14 @@
-x <- 5
-print(x)
+#penghasilan satu kecamatan
 
-sum(1:5)
-median(1,2,3,4,5)
+penghasilan <- rnorm(1000, 3000000, 250000)
+hist(penghasilan)
+
+#memasukan 1 orang penghasilan 7 juta
+baru <- 7e+06
+penghasilan.baru <- append(penghasilan, baru)
+
+median(penghasilan)
+median(penghasilan.baru)
+
+mean(penghasilan)
+mean(penghasilan.baru)
