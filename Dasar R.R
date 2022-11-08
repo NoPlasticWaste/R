39e98b608f180739ec8089f1c232a1dc10eb4d1e
Vivien <endarlan11@gmail.com>
2022-11-08 20:03:45 +0700

    Slicing dan indexing

--- Dasar R.R
+++ Dasar R.R
@@ -22,3 +22,35 @@ box1
 #semua disimpan sebagai character
 box2 <- c(12, "saya", 98.2, TRUE)
 box2
+
+angka1 <- c(10, 20, 30, 40, 50)
+angka2 <- c(5,6,7,8,9)
+angka1+angka2
+angka1/2
+
+angka3 <- c(1,2,3)
+angka3+angka2
+#error karena panjangnya tidak sama
+
+angka3 + c(4,5,6) #bisa langsung dijumlahkan dengan vektor
+
+sum(angka1)
+min(angka1)
+max(angka1)
+
+angka1 < 5
+angka1 >= 30
+
+#slicing dan indexing
+satu
+satu["A1"]
+
+angka
+angka[1:3]
+angka[c(1,2,3)]
+angka[c(1,3,5)]
+angka >= 30
+
+
+filter.saya <- angka1 >= 30
+angka2[filter.saya]
