d3ca5286a2992f898cc76b60e022901dd469ac95
Vivien <endarlan11@gmail.com>
2022-11-08 19:51:35 +0700

    Dasar R

---
+++ Dasar R.R
@@ -0,0 +1,24 @@
+# vector, tipe data satu dimensi
+satu <- c(1,2,3,4,5)
+angka <- c(1,2,3,4,5)
+
+# menset nama header
+names(satu) <- c("A","B","C","D","E")
+satu
+
+# merubah nama header
+alfabet1 <- c("A1", "A2", "A3", "A4", "A5")
+names(satu) <- alfabet1
+satu
+
+# semua disimpan sebagai numerik
+box <- c(TRUE, 23, 34.56)
+box
+
+#semua disimpan sebagai character
+box1 <- c(12, "saya", 98,2)
+box1
+
+#semua disimpan sebagai character
+box2 <- c(12, "saya", 98.2, TRUE)
+box2
